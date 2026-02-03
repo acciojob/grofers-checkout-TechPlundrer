882366eced38cbba4f2fac9ac6311e10108c5e84
Vishal Patil <vishalpatilvgp2199@gmail.com>
2026-02-03 11:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const priceCells = document.querySelectorAll(".price");
+	let total = 0;
+
+	priceCells.forEach(cell => {
+		total += Number(cell.innerText);
+	});
+
+	const table = document.querySelector("table");
+
+	if(document.querySelector(".total-row")) return;
+
+	const totalRow = document.createElement("tr");
+	totalRow.classList.add("total-row");
+
+	const totalCell = document.createElement("td");
+	totalCell.colSpan = 2;
+	totalCell.innerText = `Total Price = Rs ${total}`;
+
+	totalRow.appendChild(totalCell);
+	table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
